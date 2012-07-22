4bdb5f36fed68de2ddc13b7e35d501adb315dc38
Mateusz Adamczyk <matek_a@wp.pl>
2012-07-22 22:45:34 +0200

    Added subproject and in-project repository for rseslib interoperability

---
+++ dmexlib_rseslib/src/main/java/pl/edu/mimuw/dmexlib_rseslib/App.java
@@ -0,0 +1,13 @@
+package pl.edu.mimuw.dmexlib_rseslib;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
